7746105ef271e731b3cb9e09fa65c47e33efb0a9
Thanh An <Nguyenthanhan26.qngai@gmail.com>
2024-11-18 16:49:33 +0700

    update code

--- Zabook/src/main/java/Zabook/controllers/LoginController.java
+++ Zabook/src/main/java/Zabook/controllers/LoginController.java
@@ -5,19 +5,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RequestParam;
-=======
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
->>>>>>> 6db718dbe2cbc58b0c99306c4ce445e94c84f9e4
 
 import Zabook.models.User;
 import Zabook.services.IUserService;
 
 @Controller
-<<<<<<< HEAD
 public class LoginController {
 
     @Autowired
@@ -42,34 +35,4 @@ public class LoginController {
             return "login";
         }
     }
-=======
-@RequestMapping("/login")
-public class LoginController {
-
-	@Autowired
-	private IUserService userService;
-	
-	@GetMapping
-    public String showLoginPage() {
-        return "login";
-    }
-	
-	@PostMapping	
-    public ModelAndView login(@RequestBody User loginRequest, Model model) {
-        String username = loginRequest.getEmail();
-        String password = loginRequest.getPassword();
-
-        User user = userService.getUserByEmail(username);
-
-        if (user != null && user.getPassword().equals(password)) {
-            
-            return new ModelAndView("home");
-        } else {
-            
-            model.addAttribute("errorMessage", "Tên đăng nhập hoặc mật khẩu không đúng!");
-            return new ModelAndView("login");  
-        }
-    }
-
->>>>>>> 6db718dbe2cbc58b0c99306c4ce445e94c84f9e4
 }
